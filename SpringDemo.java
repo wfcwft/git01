0fec77baa12f3afebac464c6b3d0f77c9765b50b
Jennifer <18814230823@163.com>
2022-12-11 01:58:32 +0800

    第1次提交

---
+++ SpringDemo.java
@@ -0,0 +1,23 @@
+/**
+ * @author WongFaaCoi
+ * @project git01
+ * @user 黄花菜
+ * @date 2022年12月11日 01:53:55
+ */
+public class SpringDemo {
+    public static void main(String[] args) {
+        System.out.println("""
+                /**
+                 * @author WongFaaCoi
+                 * @project git01
+                 * @user 黄花菜
+                 * @date 2022年12月11日 01:53:55
+                 */
+                public class SpringDemo {
+                    public static void main(String[] args) {
+                        System.out.println();
+                    }
+                }
+                """);
+    }
+}
