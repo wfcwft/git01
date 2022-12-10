33c462232e3137f9f5255ccd436e4dd8d03e8ab4
Jennifer <18814230823@163.com>
2022-12-11 02:07:55 +0800

    第2次提交

--- SpringDemo.java
+++ SpringDemo.java
@@ -18,6 +18,13 @@ public class SpringDemo {
                         System.out.println();
                     }
                 }
+                文本块
+                """);
+
+        System.out.println("""
+                当前局势
+                找工作不易
+                但是我们还得继续积极生活找工作，不能自暴自弃
                 """);
     }
 }
